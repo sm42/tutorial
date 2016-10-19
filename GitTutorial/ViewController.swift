508af11cbb495b86a4b1b0a8847d9f66556fa17e
Sara McEnhill <mcenhill42@gmail.com>
2016-10-19 13:34:09 -0500

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Sara McEnhill on 10/19/16.
+//  Copyright © 2016 SaraM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
